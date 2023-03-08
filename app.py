c9dd889a5a11c52122f7bb33598b32030b38d091
Andranik Markaryan <andranik.mrk@gmail.com>
2023-03-08 23:16:24 +0000

    Configure gcloud

--- app.py
+++ app.py
@@ -55,3 +55,6 @@ def trainModel():
             del cached_images[imageId]
             f.close()
     return Response("", status=200)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
