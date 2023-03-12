8a80fb82d05dd15fb0533a800bcb503699fee490
Andranik Markaryan <andranik.mrk@gmail.com>
2023-03-12 18:41:05 +0000

    Better db setup

--- db/controllers.py
+++ db/controllers.py
@@ -2,13 +2,13 @@ import sqlite3 as sl
 import numpy as np
 from . import setup
 
-connection = sl.connect('./db/training.db', detect_types=sl.PARSE_DECLTYPES)
-
+connection = sl.connect('./db/training.db', detect_types=sl.PARSE_DECLTYPES, check_same_thread=False)
+cursor = connection.cursor()
 EXAMPLES="EXAMPLES"
 with connection:
-  output = connection.execute("SELECT name FROM sqlite_master WHERE type='table' AND name=?", [EXAMPLES])
+  output = cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name=?", [EXAMPLES])
   if output.fetchone() == None:
-    connection.execute(f"""
+    cursor.execute(f"""
             CREATE TABLE {EXAMPLES} (
                 id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                 createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
@@ -21,4 +21,5 @@ with connection:
 
 def store_new_example(image, label):
   with connection:
-    connection.execute(f"INSERT INTO {EXAMPLES} (image, label) values (?, ?)", [image, label])
+    cursor.execute(f"INSERT INTO {EXAMPLES} (image, label) values (?, ?)", [image, label])
+    connection.commit()
