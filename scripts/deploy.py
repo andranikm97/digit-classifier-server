ace189bb3e648f8518745aa02b74e21ae93e756e
Andranik Markaryan <andranik.mrk@gmail.com>
2023-04-03 00:05:45 +0100

    Add deploy script

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,3 +1,10 @@
 import os
 
-os.system('gcloud run deploy server --source ../src')
+from git import Repo
+
+current_branch = Repo('.').head.reference.name
+
+if (current_branch == "main"):
+    print('Current branch is main, please switch to prod, merge and push.')
+else:
+    os.system('git push heroku prod:main -f')
