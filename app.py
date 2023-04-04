01f854c2c3aba34c1ef1bfa467f5d5c8893cd59b
Andranik Markaryan <andranik.mrk@gmail.com>
2023-04-04 22:24:56 +0100

    Refactor server

--- app.py
+++ app.py
@@ -1,34 +1,28 @@
 import os
 import uuid
 
-import torch
 from flask import Flask, Response, make_response, request
 from flask_cors import CORS
 from PIL import Image
 
-from tools.model import Model, predict
-from tools.utils import Cache, prepareImage
-
-# from datetime import datetime
-# from pathlib import Path
-
+from model.tools import initModel, predict
+from utils import Cache, prepareImage
 
 app = Flask(__name__)
 CORS(app)
 
-model = Model()
-model.load_state_dict(torch.load("model/state.pt"))
+model = initModel()
 cache = Cache()
 
 
 @app.get("/")
 def getIndex():
-    return 'Method {}'.format(request.method)
+    return "Method {}".format(request.method)
 
 
 @app.post("/recognize")
 def parseImage():
-    data = request.files['data']
+    data = request.files["data"]
     image = prepareImage(Image.open(data))
     imageCacheId = cache.addImage(image)
 
@@ -36,12 +30,11 @@ def parseImage():
         prediction = predict(model, image)
     except Exception as e:
         prediction = -1
-        print('Prediction failed:', e)
+        print("Prediction failed:", e)
 
-    response = make_response({
-        "prediction": str(prediction),
-        "backend_id": imageCacheId
-    })
+    response = make_response(
+        {"prediction": str(prediction), "backend_id": imageCacheId}
+    )
 
     return response
 
@@ -49,19 +42,19 @@ def parseImage():
 @app.put("/train")
 def trainModel():
     data = request.json
-    imageId = uuid.UUID(data['backend_id'])
+    imageId = uuid.UUID(data["backend_id"])
     # cache.addImage(ima)
-#     correctLabel = data['label']
-#     # if imageId and cached_images[imageId]:
-#         # store_new_example(cached_images[imageId]['image'], correctLabel)
-#         # del cached_images[imageId]
+    #     correctLabel = data['label']
+    #     # if imageId and cached_images[imageId]:
+    #         # store_new_example(cached_images[imageId]['image'], correctLabel)
+    #         # del cached_images[imageId]
     return Response("", status=200)
 
 
-@app.post('/clear_image')
+@app.post("/clear_image")
 def clearImage():
     data = request.json
-    id = data['imageId']
+    id = data["imageId"]
     cache.removeImage(id)
 
 
--- utils.py
+++ utils.py
@@ -5,21 +5,17 @@ import numpy as np
 
 def prepareImage(image):
     image = image.resize((28, 28))
-    image = np.asarray(image)[:, :, 0].reshape(1, 784)
-    image = image / 255.
+    image = np.asarray(image)[:, :, 0].reshape(1, 1, 28, 28)
     return image
 
 
-class Cache():
+class Cache:
     def __init__(self) -> None:
         self.cache = {}
 
     def addImage(self, image):
         id = uuid.uuid4()
-        self.cache[id] = {
-            "image": image,
-            "label": ""
-        }
+        self.cache[id] = {"image": image, "label": ""}
         return id
 
     def removeImage(self, id):
