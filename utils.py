d3541bf0e33ccabd8d30523ea5b58316941bdde0
Andranik Markaryan <andranik.mrk@gmail.com>
2023-04-15 23:15:49 +0200

    Import model utils

--- utils.py
+++ utils.py
@@ -13,6 +13,10 @@ class Cache:
     def __init__(self) -> None:
         self.cache = {}
 
+    def getImage(self, id):
+        id = uuid.UUID(id)
+        if id in self.cache:
+            return self.cache[id]
     def addImage(self, image):
         id = uuid.uuid4()
         self.cache[id] = {"image": image, "label": ""}
